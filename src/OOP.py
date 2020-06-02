37232bd6e015760ffba5f0875bd924bc99f3f998
Tshepo Rampai <tshepojrampai@gmail.com>
2020-06-02 09:54:48 +0200

    removed unused dependencies

--- src/OOP.py
+++ src/OOP.py
@@ -1,11 +1,7 @@
 #Load dependencies
 import pandas as pd
 import numpy as np 
-import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error as mse
-from statsmodels.tools.eval_measures import rmse
-
 
 
 
